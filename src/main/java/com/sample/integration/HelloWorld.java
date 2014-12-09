ced55dcb47c54b80aca8c67407b9df1edd893fca
sdierbeck <sdierbeck@connecture.com>
2014-12-08 21:37:39 -0700

    Updated to use properties from a configuration file.

--- src/main/java/com/sample/integration/HelloWorld.java
+++ src/main/java/com/sample/integration/HelloWorld.java
@@ -1,38 +1,35 @@
 package com.sample.integration;
 
-import java.util.concurrent.TimeUnit;
-
 import com.example.data.StoreOrder;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import java.util.Properties;
  
 
 @Path("/hello")
 @Component("HelloWorld")
 public class HelloWorld {
 
+    private Properties defaultProperties;
+
     @GET
     @Path("/echo/{input}")
     @Produces("text/plain")
     @Async
     public String ping(@PathParam("input") String input) {
-    	
-    	System.out.println("Before");
-        try {
-        	Thread.sleep(10000);  
-        } catch (InterruptedException ie) {
-
-        }
-        System.out.println("Out of sleep");
+    	String serviceName = defaultProperties.getProperty("cxfsample.service.name");
+    	System.out.println("Before property is [" + serviceName + "]");
+//        try {
+//        	Thread.sleep(10000);
+//        } catch (InterruptedException ie) {
+//
+//        }
+//        System.out.println("Out of sleep");
       
-        return input;
+        return input + " " + serviceName;
     }
 
     @POST
@@ -51,6 +48,15 @@ public class HelloWorld {
     public Response storeOrder(StoreOrder order) {
 
         return Response.ok().build();
+    }
+    public void setDefaultProperties(Properties defaultProperties)
+    {
+        this.defaultProperties = defaultProperties;
+    }
+
+    public Properties getDefaultProperties()
+    {
+        return defaultProperties;
     }
 }
 
